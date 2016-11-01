6c159dfdaac1878d6c4bbf228501875704a8a63c
RossB <Ross@dev>
2016-10-31 20:14:34 -0400

    init

---
+++ Create Table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[Users](
+	[UserId] [INT] IDENTITY(1,1) NOT NULL,
+	[FirstName] [NVARCHAR](20) NOT NULL,
+	[LastName] [NVARCHAR](20) NOT NULL,
+	[Login] [NVARCHAR](50) NOT NULL,
+	[Password] [NVARCHAR](20) NOT NULL,
+	[ClientId] [INT] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)
+) ON [PRIMARY]
+
+GO
+
+
---
+++ RLS.sql
@@ -0,0 +1,27 @@
+DROP SCHEMA IF EXISTS Security
+GO
+
+CREATE SCHEMA Security
+GO
+
+DROP FUNCTION IF EXISTS Security.EntitlementCheck
+GO
+
+CREATE FUNCTION Security.EntitlementCheck(@ClientId AS INT) RETURNS TABLE  
+WITH SCHEMABINDING  
+AS  
+	RETURN SELECT 1 AS Result
+	WHERE @ClientId = SESSION_CONTEXT(N'cid')
+GO
+
+DROP SECURITY POLICY IF EXISTS dbo.EntitlementPolicy
+GO
+
+CREATE SECURITY POLICY EntitlementPolicy  
+ADD FILTER PREDICATE Security.EntitlementCheck(ClientId) ON dbo.Users
+WITH (STATE = ON)
+GO
+
+SELECT * FROM Users
+
+EXEC sp_set_session_context @key='cid', @value=1
